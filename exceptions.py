53ec1f7d122724242c5b4eaf2bba7d8f3d945362
junad75 <JUNAD75@ulaval.ca>
2023-12-04 16:52:52 -0500

    exceptions

---
+++ exceptions.py
@@ -0,0 +1,14 @@
+class ErreurDate(RuntimeError):
+    '''probleme d'une date inutilisable'''
+    def __str__(self):
+        return f'Erreur Date: {self.args}'
+
+class ErreurQuantité(RuntimeError):
+    '''probleme d'une quantité insuffisante'''
+    def __str__(self):
+        return f'Erreur Quantité: {self.args}'
+
+class LiquiditéInsuffisante(RuntimeError):
+    '''probleme de font insuffisant'''
+    def __str__(self):
+        return f'Liquidité Insuffisante: {self.args}'
